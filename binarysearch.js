cc4a65d8be944c632619a6a833120f0064b71c6e
Roshan Kumar Chaudhary <roshan2020chaudhary@gmail.com>
2026-09-01 16:24:33 +0100

    added time complexity

--- binarysearch.js
+++ binarysearch.js
@@ -3,7 +3,7 @@ function binarySearch(arr, target){
     let leftIndex= 0;
     let rightIndex= arr.length - 1;
 
-    while(leftIndex<= rightIndex){
+    while(leftIndex <= rightIndex){
         let middleIndex = Math.floor((leftIndex+rightIndex)/2);
 
         if(arr[middleIndex] === target){
@@ -18,3 +18,11 @@ function binarySearch(arr, target){
     }
      return -1;
 }
+
+console.log(binarySearch( [-5,2,4,6,10], 10)); // 4
+console.log(binarySearch( [-5,2,4,6,10], 6)); // 3
+console.log(binarySearch( [-5,2,4,6,10], 20)); // -1
+console.log(binarySearch( [-5,2,4,6,10], -5)); // 0
+console.log(binarySearch( [-5,2,4,6,10], 4)); // 2
+
+// o(log n) time complexity
